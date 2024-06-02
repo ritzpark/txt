7d0a6e63bdf33a718d77602c0073037cad907bdb
Ashik Miah <137385120+ritzpark@users.noreply.github.com>
2024-06-02 23:29:13 +0400

    Update D.py

--- D.py
+++ D.py
@@ -1,7 +1,14 @@
 import subprocess
-def get_connected_devices(): output = subprocess.check_output(["arp", "-a"]) devices = {} 
 
-for line in output.splitlines(): 
+def get_connected_devices():
+    output = subprocess.check_output(["arp", "-a"])
+    devices = {}
+    for line in output.splitlines():
+        if "(" in line:
+            split_line = line.split("(")
+            ip = split_line[0].strip()
+            mac = split_line[1].strip(")").replace(" ", ":")
+            devices[ip] = mac
+    return devices
 
-if "(" in line: split_line = line.split("(") ip = split_line[0].strip() mac = split_line[1].strip(")").replace(" ", ":") devices[ip] = mac 
-return devices
+print(get_connected_devices()) 
