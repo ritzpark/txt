196bf7c5c643d84defd54640e4de9981729af359
Ashik Miah <137385120+ritzpark@users.noreply.github.com>
2024-06-02 23:19:22 +0400

    Add files via upload

---
+++ D.py
@@ -0,0 +1,7 @@
+import subprocess
+def get_connected_devices(): output = subprocess.check_output(["arp", "-a"]) devices = {} 
+
+for line in output.splitlines(): 
+
+if "(" in line: split_line = line.split("(") ip = split_line[0].strip() mac = split_line[1].strip(")").replace(" ", ":") devices[ip] = mac 
+return devices
